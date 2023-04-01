98754c88d69bf1c83057fd1a0df899d0b74d34c6
tmgoncalves <tmgoncalves@hotmail.com>
2023-03-31 23:53:24 -0300

    primeiro aula JAVA

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imersao-java-Alura
+
+Este repositorio consite em realizar um inicio nos estudos abordando linguagem JAVA.
---
+++ alura-stickers/src/App.java
@@ -0,0 +1,43 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App{
+
+public static void main(String[] args) throws Exception{
+
+    
+    // fazer uma conex~ao HTTP e buscar os top filmes
+    // documentacao -> httprequest javadoc
+    String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+    URI endereco = URI.create(url);
+    var client = HttpClient.newHttpClient();
+    var request = HttpRequest.newBuilder(endereco).GET().build();
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    String body = response.body();
+    // extrair somente os dados que interessam (titulo, poster, ckassificacao)
+    // essa etapa e conhecida como parser.
+    // uma das maneiras de limpar os dados e a utilizacao e o Regex
+    // site regex: regex101.com
+    var parser = new JsonParser();
+    List<Map<String, String>> listaDeFilmes = parser.parse(body);
+    //System.out.println(listaDeFilmes.size());
+    //System.out.println(listaDeFilmes.get(0));
+
+    // exibir e manipular os dados
+    for (Map<String,String> filme : listaDeFilmes) {
+        System.out.println(filme.get("title"));
+        System.out.println(filme.get("image"));
+        System.out.println(filme.get("imDbRating"));
+
+        System.out.println();
+        
+    }
+
+    }
+}
